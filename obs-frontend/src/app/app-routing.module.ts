45d55db7595d6334dd14f9384ed1d9f642e28f67
ozankocali <ozan.kocali@hotmail.com>
2021-03-31 11:36:06 +0300

    routing and navbar added

--- obs-frontend/src/app/app-routing.module.ts
+++ obs-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentListComponent } from './student-list/student-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"students",pathMatch:"full"},
+  {path:"students",component:StudentListComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
